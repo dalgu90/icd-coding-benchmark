90ca8cb65d052766cbc83924f866c093f75714d8
Juyong Kim <dalgu90@gmail.com>
2022-02-02 00:45:24 -0500

    Change dataset to output token idxs and one-hot labels

--- src/datasets/base_dataset.py
+++ src/datasets/base_dataset.py
@@ -1,16 +1,27 @@
+import numpy as np
 import pandas as pd
+import torch
 from torch.utils.data import Dataset
 
 from src.utils.file_loaders import load_json
-from src.utils.mapper import configmapper
+from src.utils.mapper import ConfigMapper
 
 
-@configmapper("datasets", "base_dataset")
+@ConfigMapper.map("datasets", "base_dataset")
 class BaseDataset(Dataset):
     def __init__(self, config):
         self._config = config
         data_path = self._config.data_file
+
+        # Load vocab (dict of {word: idx})
+        self.vocab = load_json(config.vocab_file)
+        self.vocab_size = len(self.vocab)
+        assert self.vocab_size == max(self.vocab.values()) + 1
+
+        # Load labels (dict of {code: idx})
         self.all_labels = load_json(config.label_file)
+        self.num_labels = len(self.all_labels)
+        assert self.num_labels == max(self.all_labels.values()) + 1
 
         # To-do: This class currently deals with only CSV files. We can extend
         # this to deal with other file types (.json, .xlsx, etc.).
@@ -23,8 +34,32 @@ class BaseDataset(Dataset):
     def __getitem__(self, idx):
         row = self.df.iloc[idx]
         clinical_note = row[self._config.column_names.clinical_note]
-        labels = row[self._config.column_names.label].split(";")
+        codes = row[self._config.column_names.label].split(";")
 
-        # convert labels to indices
-        labels = [self.all_labels[label] for label in labels]
+        # Note -> word idxs (UNK is assigned after the last word)
+        clinical_note = [self.vocab[w] if w in self.vocab else self.vocab_size
+                         for w in clinical_note.split()]
+
+        # ICD codes -> binary labels
+        labels = np.zeros(self.num_labels, dtype=np.int32)
+        for code in codes:
+            labels[self.all_labels[code]] = 1
+
         return (clinical_note, labels)
+
+    def get_collate_fn(self):
+        def _collate_fn(examples):
+            notes, labels = zip(*examples)
+
+            # Pad notes
+            max_note_len = max(map(len, notes))
+            notes = [note + [0] * (max_note_len - len(note)) for note in notes]
+
+            # Convert into Tensor
+            notes = torch.tensor(notes)
+            labels = torch.tensor(labels)
+
+            return notes, labels
+        return _collate_fn
+
+
