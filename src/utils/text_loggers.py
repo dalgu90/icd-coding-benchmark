fa039b873e293c48b924c32e94e43f078732bda6
abheesht17 <sharmabhee@gmail.com>
2022-03-02 19:37:10 +0530

    Bug fix

--- src/utils/text_loggers.py
+++ src/utils/text_loggers.py
@@ -2,17 +2,18 @@ import logging
 import sys
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
-file_handler = logging.FileHandler("logs/logs.log")
+file_handler = logging.FileHandler("logs/log.log")
 file_handler.setFormatter(
-    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
+    logging.Formatter("%(asctime)s:%(levelname)s:%(module)s: %(message)s")
 )
-file_handler.setLevel(logging.DEBUG)
 logger.addHandler(file_handler)
 
+
+logger.setLevel(logging.INFO)
 stdout_handler = logging.StreamHandler(sys.stdout)
 stdout_handler.setFormatter(
-    logging.Formatter("\n%(asctime)s:%(levelname)s:%(name)s: %(message)s")
+    logging.Formatter("\n%(asctime)s:%(levelname)s:%(module)s: %(message)s")
 )
-stdout_handler.setLevel(logging.INFO)
 logger.addHandler(stdout_handler)
