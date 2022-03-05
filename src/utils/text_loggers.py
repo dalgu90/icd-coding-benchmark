cc4073e62b90208fc5c17782a91decbb0398f4a1
abheesht17 <sharmabhee@gmail.com>
2022-03-05 08:09:02 +0530

    Bug fixes

--- src/utils/text_loggers.py
+++ src/utils/text_loggers.py
@@ -1,29 +1,31 @@
 import logging
 import sys
-from logging.handlers import FileHandler
+from datetime import datetime
 
+TIME_SINCE_EPOCH = datetime.timestamp(datetime.now())
 FORMATTER = logging.Formatter(
     "%(asctime)s — %(name)s — %(levelname)s — %(message)s"
 )
-LOG_FILE = "logs/log_file.log"
+LOG_FILE = f"logs/{TIME_SINCE_EPOCH}.log"
 
 
 def get_console_handler():
     console_handler = logging.StreamHandler(sys.stdout)
     console_handler.setFormatter(FORMATTER)
-    console_handler.setLevel(logging.DEBUG)
+    console_handler.setLevel(logging.INFO)
     return console_handler
 
 
 def get_file_handler():
-    file_handler = FileHandler(LOG_FILE)
+    file_handler = logging.FileHandler(LOG_FILE)
     file_handler.setFormatter(FORMATTER)
-    file_handler.setLevel(logging.INFO)
+    file_handler.setLevel(logging.DEBUG)
     return file_handler
 
 
 def get_logger(logger_name):
     logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
     logger.addHandler(get_console_handler())
     logger.addHandler(get_file_handler())
     logger.propagate = False
