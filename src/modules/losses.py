5c60ba9eeceb82379e1cf9e21ab157ba0488ac22
abheesht17 <sharmabhee@gmail.com>
2022-03-23 12:32:14 +0530

    Add BCE with  label smoothing loss

--- src/modules/losses.py
+++ src/modules/losses.py
@@ -19,3 +19,18 @@ class BinaryCrossEntropyLoss(BCEWithLogitsLoss):
         if target.dtype != torch.float:
             target = target.float()
         return super().forward(input=input, target=target)
+
+
+@ConfigMapper.map("losses", "BinaryCrossEntropyWithLabelSmoothingLoss")
+class BinaryCrossEntropyWithLabelSmoothingLoss(BCEWithLogitsLoss):
+    def __init__(self, config):
+        self.config = config
+        super().__init__(**(config.as_dict() if config else {}))
+
+    def forward(self, input, target):
+        if target.dtype != torch.float:
+            target = target.float()
+        target = target * (
+            1 - self.config.alpha
+        ) + self.config.alpha / target.size(1)
+        return super().forward(input=input, target=target)
