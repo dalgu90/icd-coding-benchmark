c4b59e395dd19c9b4b7478f6280fae7dc00f2b1c
Juyong Kim <dalgu90@gmail.com>
2022-05-14 18:49:37 -0400

    Update demo app and config

--- app.py
+++ app.py
@@ -23,12 +23,7 @@ from src.modules.embeddings import *
 from src.modules.preprocessors import ClinicalNotePreprocessor
 from src.utils.checkpoint_savers import *
 from src.utils.configuration import Config
-from src.utils.file_loaders import load_json
 from src.utils.mapper import ConfigMapper
-
-# Title
-st.title("ICD Coding Benchmark Demo")
-status = st.empty()  # Displaying status
 
 hash_funcs = {
     Config: lambda x: hash(str(x)),
@@ -36,7 +31,7 @@ hash_funcs = {
 }
 
 
-@st.cache(hash_funcs=hash_funcs)
+@st.cache(hash_funcs=hash_funcs, allow_output_mutation=True)
 def load_config():
     parser = argparse.ArgumentParser(description="Here we put app desc")
     parser.add_argument(
@@ -50,83 +45,95 @@ def load_config():
     return config
 
 
-@st.cache(hash_funcs=hash_funcs)
+@st.cache(hash_funcs=hash_funcs, allow_output_mutation=True)
 def load_modules(config):
     # Load preprocessor
-    preprocessor = ClinicalNotePreprocessor(
-        config.model.clinical_note_preprocessing
+    preprocessor = ClinicalNotePreprocessor(config.clinical_note_preprocessing)
+    preprocessor("")
+
+    # Load dataset
+    dataset = ConfigMapper.get_object("datasets", config.dataset.name)(
+        config.dataset.params
     )
-
-    # Load vocab (dict of {word: idx})
-    embedding_cls = ConfigMapper.get_object("embeddings", "word2vec")
-    vocab = embedding_cls.load_vocab(config.model.word2vec_dir)
-    vocab_size = len(vocab)
-    inv_vocab = {i: k for k, i in vocab.items()}
-    assert vocab_size == max(vocab.values()) + 1
-
-    # Load labels
-    labels = load_json(config.model.label_file)
-    labels = list(zip(*sorted(labels.items(), key=lambda x: x[1])))[0]
-
-    # Icd description
-    icd_desc = list(csv.reader(open(config.demo.icd_desc_file), delimiter="\t"))
-    icd_desc = {r[0]: r[1] for r in icd_desc}
 
     # Load model
     model = ConfigMapper.get_object("models", config.model.name)(
         config.model.params
     )
+
+    # Restore ckpt
     ckpt_saver = ConfigMapper.get_object(
-        "checkpoint_savers", config.model.checkpoint_saver.name
-    )(config.model.checkpoint_saver.params)
+        "checkpoint_savers", config.checkpoint_saver.name
+    )(config.checkpoint_saver.params)
     best_ckpt = ckpt_saver.get_best_checkpoint()
     if best_ckpt is None:
         raise ValueError("Best ckpt not found")
     ckpt_saver.load_ckpt(model, best_ckpt, optimizer=None)
+
+    # Set model GPU & eval mode
+    if config.demo.use_gpu:
+        model.cuda()
     model.eval()
 
-    # if config.demo.use_gpu:
-    # model.cuda()
-
-    return preprocessor, vocab, inv_vocab, labels, icd_desc, model
+    return preprocessor, dataset, model
 
 
-status.text("Loading model...")
+@st.cache(hash_funcs=hash_funcs, allow_output_mutation=True)
+def load_icd_desc(config):
+    # Icd description
+    icd_desc = list(csv.reader(open(config.demo.icd_desc_file), delimiter="\t"))
+    icd_desc = {r[0]: r[1] for r in icd_desc}
+    return icd_desc
+
+
+# Title
+st.title("ICD Coding Interactive Demo")
+status = st.empty()  # Displaying status
+
+# Load config, modules, and icd descriptions
 config = load_config()
-preprocessor, vocab, inv_vocab, labels, icd_desc, model = load_modules(config)
+preprocessor, dataset, model = load_modules(config)
+icd_desc = load_icd_desc(config)
 status.text(f"Model loaded ({config.model.name})")
 
 # Input text
-text = st.text_area(label="Discharge summary note", height=200)
+input_text = st.text_area(label="Discharge summary note", height=200)
 
-# Preprocessed text
-text2 = preprocessor(text)
-st.text_area(label="Preprocessed note", value=text2, height=200, disabled=True)
+# Preprocess text
+preprocessed_text = preprocessor(input_text)
+st.text_area(
+    label="Preprocessed note",
+    value=preprocessed_text,
+    height=200,
+    disabled=True,
+)
 
-# Token indices
-unk_idx = vocab[config.model.unk_token]
-text3 = [vocab[w] if w in vocab else unk_idx for w in text2.split()]
-text3 = text3[: config.model.max_length]
-text4 = " ".join([inv_vocab[idx] for idx in text3])
-st.text_area(label="Tokens", value=text4, height=200, disabled=True)
+# Tokenize text with vocab
+token_idxs = dataset.encode_tokens(preprocessed_text.split())
+token_text = " ".join(dataset.decode_tokens(token_idxs))
+st.text_area(label="Tokens", value=token_text, height=200, disabled=True)
 
+# Model prediction
 st.write("Output")
-if text3:
+if token_idxs:
     # Forward pass
-    batch_input = torch.tensor([text3])
-    # if config.demo.use_gpu:
-    # batch_input = batch_input.cuda()
+    batch_input = torch.tensor([token_idxs])
+    if config.demo.use_gpu:
+        batch_input = batch_input.cuda()
     with torch.no_grad():
         batch_output = model(batch_input)
     probs = torch.sigmoid(batch_output[0].cpu()).numpy()
-    top_k_pred = np.argsort(probs)[-1 : -(config.demo.top_k + 1) : -1]
+    top_k_preds = np.argsort(probs)[-1 : -(config.demo.top_k + 1) : -1]
+    top_k_probs = [probs[p] for p in top_k_preds]
+    top_k_codes = dataset.decode_labels(top_k_preds)
+    top_k_descs = [icd_desc[c] for c in top_k_codes]
 
-    # Outputs
+    # Output as table
     output_df = pd.DataFrame(
         {
-            "ICD-9 Code": [labels[p] for p in top_k_pred],
-            "Probability": [probs[p] for p in top_k_pred],
-            "Description": [icd_desc[labels[p]] for p in top_k_pred],
+            "ICD-9 Code": top_k_codes,
+            "Probability": top_k_probs,
+            "Description": top_k_descs,
         }
     )
     st.dataframe(output_df.style.format({"Probability": "{:.4f}"}))
