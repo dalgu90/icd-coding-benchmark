3a73a51e5cd56a33d165ccff480487e4ed53ce5b
Juyong Kim <dalgu90@gmail.com>
2022-05-28 01:30:01 -0400

    Resolve an import warning

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,4 @@
 from src.models.caml import ConvAttnPool as CAML
 from src.models.caml import VanillaConv as CNN
-from src.models.multirescnn import MultiResCNN as MultiResCNN
+from src.models.multirescnn import MultiResCNN
 from src.models.dcan import DCAN
-
