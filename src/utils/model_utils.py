578c2aca381886e0fdcd00a8d0a8197a7dd6f537
Juyong Kim <dalgu90@gmail.com>
2022-06-02 10:36:05 -0400

    Remove a function in model_util.py and run precommit

--- src/utils/model_utils.py
+++ src/utils/model_utils.py
@@ -1,16 +1,18 @@
 """
     Model utils
 """
+import codecs
 import csv
 import os
+import re
 from collections import defaultdict
+
+import numpy as np
 
 from src.modules.preprocessors import CodeProcessor
 from src.utils.file_loaders import load_csv_as_df, load_json
 from src.utils.mapper import ConfigMapper
-import numpy as np
-import codecs
-import re
+
 
 def load_lookups(
     dataset_dir,
@@ -104,7 +106,6 @@ def pad_desc_vecs(desc_vecs):
     return pad_vecs
 
 
-
 def _readString(f, code):
     # s = unicode()
     s = str()
@@ -112,7 +113,7 @@ def _readString(f, code):
     value = ord(c)
 
     while value != 10 and value != 32:
-        if 0x00 < value < 0xbf:
+        if 0x00 < value < 0xBF:
             continue_to_read = 0
         elif 0xC0 < value < 0xDF:
             continue_to_read = 1
@@ -130,7 +131,7 @@ def _readString(f, code):
         temp = bytes()
         temp = temp + c
 
-        while i<continue_to_read:
+        while i < continue_to_read:
             temp = temp + f.read(1)
             i += 1
 
@@ -142,17 +143,19 @@ def _readString(f, code):
 
     return s
 
+
 import struct
+
+
 def _readFloat(f):
     bytes4 = f.read(4)
-    f_num = struct.unpack('f', bytes4)[0]
+    f_num = struct.unpack("f", bytes4)[0]
     return f_num
-
 
 
 def norm2one(vec):
     root_sum_square = np.sqrt(np.sum(np.square(vec)))
-    return vec/root_sum_square
+    return vec / root_sum_square
 
 
 def load_pretrain_emb(embedding_path):
@@ -160,13 +163,13 @@ def load_pretrain_emb(embedding_path):
     embedd_dict = dict()
 
     # emb_debug = []
-    if embedding_path.find('.bin') != -1:
-        with open(embedding_path, 'rb') as f:
-            wordTotal = int(_readString(f, 'utf-8'))
-            embedd_dim = int(_readString(f, 'utf-8'))
+    if embedding_path.find(".bin") != -1:
+        with open(embedding_path, "rb") as f:
+            wordTotal = int(_readString(f, "utf-8"))
+            embedd_dim = int(_readString(f, "utf-8"))
 
             for i in range(wordTotal):
-                word = _readString(f, 'utf-8')
+                word = _readString(f, "utf-8")
                 # emb_debug.append(word)
 
                 word_vector = []
@@ -179,7 +182,7 @@ def load_pretrain_emb(embedding_path):
                 embedd_dict[word] = word_vector
 
     else:
-        with codecs.open(embedding_path, 'r', 'UTF-8') as file:
+        with codecs.open(embedding_path, "r", "UTF-8") as file:
             for line in file:
                 # logging.info(line)
                 line = line.strip()
@@ -188,7 +191,7 @@ def load_pretrain_emb(embedding_path):
                 # tokens = line.split()
                 tokens = re.split(r"\s+", line)
                 if len(tokens) == 2:
-                    continue # it's a head
+                    continue  # it's a head
                 if embedd_dim < 0:
                     embedd_dim = len(tokens) - 1
                 else:
@@ -199,62 +202,4 @@ def load_pretrain_emb(embedding_path):
                 embedd[:] = tokens[1:]
                 embedd_dict[tokens[0]] = embedd
 
-
     return embedd_dict, embedd_dim
-
-def build_pretrain_embedding(embedding_path, word_alphabet, norm):
-
-    embedd_dict, embedd_dim = load_pretrain_emb(embedding_path)
-
-    scale = np.sqrt(3.0 / embedd_dim)
-    pretrain_emb = np.zeros([len(word_alphabet)+2, embedd_dim], dtype=np.float32)  # add UNK (last) and PAD (0)
-    perfect_match = 0
-    case_match = 0
-    digits_replaced_with_zeros_found = 0
-    lowercase_and_digits_replaced_with_zeros_found = 0
-    not_match = 0
-    for word, index in word_alphabet.items():
-        if word in embedd_dict:
-            if norm:
-                pretrain_emb[index,:] = norm2one(embedd_dict[word])
-            else:
-                pretrain_emb[index,:] = embedd_dict[word]
-            perfect_match += 1
-
-        elif word.lower() in embedd_dict:
-            if norm:
-                pretrain_emb[index,:] = norm2one(embedd_dict[word.lower()])
-            else:
-                pretrain_emb[index,:] = embedd_dict[word.lower()]
-            case_match += 1
-
-        elif re.sub('\d', '0', word) in embedd_dict:
-            if norm:
-                pretrain_emb[index,:] = norm2one(embedd_dict[re.sub('\d', '0', word)])
-            else:
-                pretrain_emb[index,:] = embedd_dict[re.sub('\d', '0', word)]
-            digits_replaced_with_zeros_found += 1
-
-        elif re.sub('\d', '0', word.lower()) in embedd_dict:
-            if norm:
-                pretrain_emb[index,:] = norm2one(embedd_dict[re.sub('\d', '0', word.lower())])
-            else:
-                pretrain_emb[index,:] = embedd_dict[re.sub('\d', '0', word.lower())]
-            lowercase_and_digits_replaced_with_zeros_found += 1
-
-        else:
-            if norm:
-                pretrain_emb[index, :] = norm2one(np.random.uniform(-scale, scale, [1, embedd_dim]))
-            else:
-                pretrain_emb[index,:] = np.random.uniform(-scale, scale, [1, embedd_dim])
-            not_match += 1
-
-    # initialize pad and unknown
-    pretrain_emb[0, :] = np.zeros([1, embedd_dim], dtype=np.float32)
-    if norm:
-        pretrain_emb[-1, :] = norm2one(np.random.uniform(-scale, scale, [1, embedd_dim]))
-    else:
-        pretrain_emb[-1, :] = np.random.uniform(-scale, scale, [1, embedd_dim])
-
-    return pretrain_emb, embedd_dim
-
