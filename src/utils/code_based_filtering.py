b65edd68d972e99f7ede2f382a153eafa6ed80df
abheesht17 <sharmabhee@gmail.com>
2022-06-23 16:49:01 +0530

    Fix top-k code

--- src/utils/code_based_filtering.py
+++ src/utils/code_based_filtering.py
@@ -24,23 +24,18 @@ class TopKCodes:
         self.labels_freq_save_path = labels_freq_save_path
 
     def __call__(self, label_col_name, code_df):
-        self.find_top_k_codes(label_col_name, code_df)
+        label_counts = self.find_top_k_codes(label_col_name, code_df)
+
         save_json(
             {v: k for k, v in enumerate(self.top_k_codes)},
             self.labels_save_path,
         )
 
-        label_freq = {k: 0 for k in self.top_k_codes}
+        if self.labels_freq_save_path is not None:
+            save_json(label_counts, self.labels_freq_save_path)
 
         if self.k == 0:
-            if self.labels_freq_save_path:
-                for idx, row in code_df.iterrows():
-                    codes = set(row[label_col_name].split(";"))
-                    for code in codes:
-                        label_freq[code] += 1
-                save_json(label_freq, self.labels_freq_save_path)
             return code_df
-
         indices_to_delete = []
         top_k_codes_set = set(self.top_k_codes)
         for idx, row in code_df.iterrows():
@@ -48,23 +43,16 @@ class TopKCodes:
                 top_k_codes_set
             )
             if len(filtered_indices) > 0:
-                if self.labels_freq_save_path is not None:
-                    for filtered_idx in filtered_indices:
-                        label_freq[filtered_idx] += 1
                 row[label_col_name] = ";".join(filtered_indices)
             else:
                 indices_to_delete.append(idx)
-
-        if self.labels_freq_save_path is not None:
-            save_json(label_freq, self.labels_freq_save_path)
-
         code_df.drop(indices_to_delete, inplace=True)
         return code_df
 
     def find_top_k_codes(self, label_col_name, code_df):
         counts = Counter()
         for _, row in code_df.iterrows():
-            for label in row[label_col_name].split(";"):
+            for label in set(row[label_col_name].split(";")):
                 counts[label] += 1
         if self.k == 0:
             self.top_k_codes = [code for code, _ in counts.items()]
@@ -73,5 +61,7 @@ class TopKCodes:
 
         logger.debug("top-k codes: {}".format(self.top_k_codes))
 
-    def find_code_freq(self, label_lst):
-        mlb = MultiLabelBinarizer(classes=label_lst)
+        if self.k != 0:
+            counts = counts.most_common(self.k)
+        counts = dict(counts)
+        return counts
